9fbe7a28777333417e815235ffe4fbb88101b090
Shavkat <shavkatkurbanov065@gmail.com>
2023-08-18 16:01:12 +0500

    Store сайт готов.

---
+++ products/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from products.models import Basket, Product, ProductCategory
+
+
+# admin.site.register(ProductCategory)
+@admin.register(ProductCategory)
+class ProductsCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    fields = ('name', 'description')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'quantity', 'category')
+    fields = ('image', 'name', 'description', ('price', 'quantity'), 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 0
